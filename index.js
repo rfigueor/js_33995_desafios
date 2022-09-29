acd52fa4e908aab33320560e4538609f7acf5373
Renato Figueroa Ortega <rfigueor@gmail.com>
2022-09-28 23:13:40 -0300

    incoporacion de libreria sweetalert2

--- index.js
+++ index.js
@@ -1,73 +1,188 @@
-function fnPintura(){
-    let mt2=0;
-    let totalMonto=0;
-    let precio = 2000;
-    
-    let tipoPinturaTrabajo = parseInt(prompt("¿Que desea pintar?\n1. Casa (Obs: 1 Solo piso) \n2.Edificio (Obs: Se agrega 5% extra al valor del mt2 por cada piso a contar desde 2do piso)"));    
+const servicios = [
+    { id: 1, nombre: "Trabajo de Pintura" },
+    { id: 2, nombre: "Construcción de Radier" },
+]
 
-    //Se ejecuta el ciclo hasta que selecciona una opcion valida
-    while (tipoPinturaTrabajo !== 1 &&  tipoPinturaTrabajo !== 2){
-        tipoTrabajo = parseInt(prompt("¿Que desea pintar?\n1. Casa (Obs: 1 Solo piso) \n2.Edificio (Obs: Se agrega 5% extra al valor del mt2 por cada piso a contar desde 2do piso)"));    
+const pintura = [
+    { id: 1, nombre: "Casa", nota: "(Obs: 1 Solo piso)" },
+    { id: 2, nombre: "Edificio", nota: "(Obs: Se agrega 5% extra al valor del mt2 por cada piso a contar desde 2do piso)" },
+]
+
+const mtXvalor = (mt, valor) => { return mt * valor; }
+
+const extraXpiso = (porc, numPiso, precio) => { return ((precio / 100) * (porc * numPiso)) + precio }
+
+function fnBuildMessage(item) {
+    let msg = "";
+
+    switch (item) {
+        case "servicios":
+            let lstServicio = JSON.parse(localStorage.getItem("ltServicios"));
+
+            msg = `<option value="0" selected>Seleccione Servicio</option>`
+
+            for (const item of lstServicio) {
+                msg = msg + `<option value="${item.id}">${item.nombre}</option>`
+            }
+            document.getElementById("listServicio").innerHTML = msg;
+            break;
+
+        case "pintura":
+            let lstPintura = JSON.parse(localStorage.getItem("ltPintura"));
+            msg = `<option value="0" selected>Seleccione Superficie a Pintar</option>`
+
+            for (const item of lstPintura) {
+
+                msg = msg + `<option value="${item.id}">${item.nombre} ${item.nota}</option>`
+            }
+            document.getElementById("listPintura").innerHTML = msg;
+            break;
+
+        default:
+            msg = "sin informacion";
+            break;
     }
+    return msg;
+}
 
+async function fnCalcular() {
+    let msg = '';
+    let mt2 = 0;              // Inicializa variable
+    let totalMonto = 0;       // Inicializa variable
+    let tipoServicio = Number(localStorage.getItem("lsServicioSelect"));
+    let precio = tipoServicio === 1 ? 2000 : 2500;      // Valor de mano de obra por metro cuadrado
+    
+    //SERVICIO DE PINTURA
+    if (tipoServicio === 1) {
 
-    if(tipoPinturaTrabajo===1){
-        mt2 = parseInt(prompt("¿Cantidad de metros cuadrados?"));    
-        totalMonto= precio * mt2;
-        alert("El valor total del trabajo de pintura de la casa es de : $"+ totalMonto.toString());
-    }else{
-        let porcAdic = 5;
-        let pisos = parseInt(prompt("¿Cantidad de pisos del edificio?"));
-        while (pisos === 1){
-            pisos = parseInt(prompt("La cantidad de pisos debe ser igual o mayor a 2:"));
+        let tipoTrabajo = Number(localStorage.getItem("lsTipoTrabajoSelect"));
+
+        if (tipoTrabajo === 1) {
+            mt2 = Number(localStorage.getItem("mtCasa"));
+            msg = "El valor total del trabajo de pintura de la casa es de : $" + mtXvalor(mt2, precio).toString();
+        } else if (tipoTrabajo === 2) {
+            let porcAdic = 5;
+            let pisos = Number(localStorage.getItem("pisosEdif"));
+            mt2 = Number(localStorage.getItem("mtEdif"));
+
+            //Se realiza calculo del piso 1
+            totalMonto = mtXvalor(mt2, precio);
+            for (let x = 1; x < pisos; x++) {
+                totalMonto = totalMonto + mtXvalor(mt2, extraXpiso(porcAdic, x, precio));
+            }
+            msg = "El valor total del trabajo de pintura del edificio es de $" + totalMonto.toString();
         }
-        mt2 = parseInt(prompt("¿Cantidad de metros cuadrados?"));    
+    } else if (tipoServicio === 2) {
+        //SERVICIO DE RADIER
+        let mtCubicosXsaco = 0.025;       // Se entienede que un 1 saco rinde para 1 mt cuadrado con una altura de 25 cm 
 
-        //Se realiza calculo del piso 1
-        totalMonto= precio * mt2;
+        mt2 = Number(localStorage.getItem("mtRadier"));
+        let cmAltura = Number(localStorage.getItem("cmRadier"));
 
-        //Para efectos del calculo (multiplicacion), se inicia en 1 pero se llega a la cantidad "anterior" del total de piso [x < pisos]
-        for (let x = 1; x < pisos; x++) {
-            totalMonto = totalMonto + ((((precio / 100) * (porcAdic*x)) + precio) * mt2);
-        }
+        let cantSacos = Math.round((mt2 * (cmAltura / 1000)) / mtCubicosXsaco);
 
-        alert("El valor total del trabajo de pintura del edificio es de $"+ totalMonto.toString());
+        msg = "El valor del trabajo es de $" + mtXvalor(mt2, precio).toString() + " y se requieren un total de " + cantSacos.toString() + " sacos de concreto.";
+    } else {
+        msg = "Servicio seleccionado no valido";
     }
+    console.log(msg);
+
+    Swal.fire({
+        icon: 'info',
+        title: 'Monto del Trabajo',
+        text: msg,
+      });
 }
 
 
-function fnRadier(){
-    let mt2 = parseInt(prompt("¿Cantidad de metros cuadrados?"));    
-    let cmAltura= parseInt(prompt("¿Cantidad de cm del altura?"));
-    
-    
-    while (cmAltura < 15){
-        cmAltura = parseInt(prompt("La altura minima necesaria es de 15 cm, favor ingrese nuevamente la altura:"));
+
+document.getElementById('listServicio').addEventListener('change', async function () {
+
+    let tipoServicio = Number(this.value);
+    localStorage.setItem("lsServicioSelect", tipoServicio);
+
+    if (tipoServicio !== 1 && tipoServicio !== 2){
+        document.getElementById("divPintura").style.visibility = "hidden";
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Seleccione Opcion Valida!!',
+          })
     }
+    else if(tipoServicio === 1){
+        document.getElementById("divPintura").style.visibility = (tipoServicio === 1) ? "visible" : "hidden";
+    }
+    else if(tipoServicio === 2){
+        const { value: mt2 } = await Swal.fire({
+            title: '¿Cantidad de metros cuadrados?',
+            input: 'number',
+            inputPlaceholder: 'Ingrese cantidad de metros cuadrados'
+          });
 
-    let mtCubicosXsaco=0.025;       // Se entienede que un 1 saco rinde para 1 mt cuadrado con una altura de 25 cm 
-    let precio = 2500;              // Valor de mano de obra por metro cuadrado
+          const { value: cm } = await Swal.fire({
+            title: '¿Altura en cm del radier? (cm)',
+            input: 'number',
+            inputPlaceholder: 'Ingrese altura del radier en cm'
+          });
 
-    let cantSacos = Math.round((mt2 * (cmAltura/1000))/mtCubicosXsaco);
-    let totalMonto= mt2 * precio;
+          localStorage.setItem("mtRadier", mt2);
+          localStorage.setItem("cmRadier", cm);
 
-    alert("- Se requieren un total de "+ cantSacos.toString()+" sacos de concreto.\n- El valor del trabajo es de $"+ totalMonto.toString());
+          fnCalcular();
+    }
+});
+
+
+document.getElementById('listPintura').addEventListener('change', async function () {
+
+    let tipoTrabajo = Number(this.value);
+    localStorage.setItem("lsTipoTrabajoSelect", tipoTrabajo);
+
+    if (tipoTrabajo !== 1 && tipoTrabajo !== 2){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Seleccione Opcion Valida!!',
+          })
+    }
+    else if(tipoTrabajo === 1){
+        const { value: mt2 } = await Swal.fire({
+            title: '¿Cantidad de metros cuadrados?',
+            input: 'number',
+            inputPlaceholder: 'Ingrese cantidad de metros cuadrados'
+          });
+          localStorage.setItem("mtCasa", mt2);
+          fnCalcular();
+    }
+    else if(tipoTrabajo === 2){
+        const { value: pisos } = await Swal.fire({
+            title: '¿Cantidad de pisos?',
+            input: 'number',
+            inputPlaceholder: 'Ingrese cantidad de pisos del edificio'
+          });
+
+          const { value: mt2 } = await Swal.fire({
+            title: '¿Cantidad de metros cuadrados ?',
+            input: 'number',
+            inputPlaceholder: 'Ingrese cantidad de metros cuadrados'
+          });
+
+          localStorage.setItem("pisosEdif", pisos);
+          localStorage.setItem("mtEdif", mt2);
+
+          fnCalcular();
+    }
+});
+
+function fnStar() {
+
+    localStorage.setItem("ltServicios", JSON.stringify(servicios));
+    localStorage.setItem("ltPintura", JSON.stringify(pintura));
+
+    document.getElementById("divPintura").style.visibility = "hidden";
     
+    fnBuildMessage("servicios");
+    fnBuildMessage("pintura");
 }
 
-
-let tipoTrabajo = parseInt(prompt("¿Seleccione el tipo de trabajo a realizar?\n1. Trabajo de Pintura\n2. Construcción de Radier"));
-
-//Se ejecuta el ciclo hasta que selecciona una opcion valida
-while (tipoTrabajo !== 1 &&  tipoTrabajo !== 2){
-    tipoTrabajo = parseInt(prompt("Debe seleccionar el tipo de trabajo:\n1. Trabajo de Pintura\n2. Construcción de Radier"));
-}
-
-if(tipoTrabajo === 1)
-{
-    fnPintura();
-}
-else
-{
-    fnRadier();
-}
+fnStar()
